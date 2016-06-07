c396415809ca2ffa1f6bc2136cb45c12131b9e39
Matt Pindell <pindell.m@gmail.com>
2016-06-07 14:31:14 -0600

    Add Wallets table and Wallet model

---
+++ db/migrate/20160607203003_create_wallets.rb
@@ -0,0 +1,10 @@
+class CreateWallets < ActiveRecord::Migration
+  def change
+    create_table :wallets do |t|
+      t.references :user, index: true, foreign_key: true
+      t.text :private_key
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606183341) do
+ActiveRecord::Schema.define(version: 20160607203003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,4 +34,14 @@ ActiveRecord::Schema.define(version: 20160606183341) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  create_table "wallets", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "private_key"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "wallets", ["user_id"], name: "index_wallets_on_user_id", using: :btree
+
+  add_foreign_key "wallets", "users"
 end
