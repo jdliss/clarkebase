6f69db9e23fd298f6a0e570499332bf21ac24d40
Matt Pindell <pindell.m@gmail.com>
2016-06-07 15:09:49 -0600

    Add before_action method to Wallet Model

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,9 +1,8 @@
-require_relative '../services/pkey_service'
-
 class Wallet < ActiveRecord::Base
   belongs_to :user
+  before_save :generate_private_key
 
-  def private_key
-    PKeyService.generate_private_key
+  def generate_private_key
+    self.private_key ||= PKeyService.generate_private_key
   end
 end
