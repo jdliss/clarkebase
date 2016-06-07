6d439788289565a7949ba89220ba4d0efdc51eec
Matt Pindell <pindell.m@gmail.com>
2016-06-07 15:09:59 -0600

    Add shoulda-callback-matchers to Wallet spec

--- spec/models/wallet_spec.rb
+++ spec/models/wallet_spec.rb
@@ -2,11 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Wallet, type: :model do
   it { should belong_to :user }
-
-  it "can generate a new private key" do
-    wallet      = Wallet.new
-    private_key = wallet.private_key
-
-    expect(private_key.class).to eq String
-  end
+  it { is_expected.to callback(:generate_private_key).before(:save) }
 end
