817b4f087afb8229439dc8ff96c4cdf3a1182da0
Matt Pindell <pindell.m@gmail.com>
2016-06-07 15:00:33 -0600

    Add Wallet spec

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,3 +1,9 @@
+require_relative '../services/pkey_service'
+
 class Wallet < ActiveRecord::Base
   belongs_to :user
+
+  def private_key
+    PKeyService.generate_private_key
+  end
 end
--- spec/models/wallet_spec.rb
+++ spec/models/wallet_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Wallet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :user }
+
+  it "can generate a new private key" do
+    wallet      = Wallet.new
+    private_key = wallet.private_key
+
+    expect(private_key.class).to eq String
+  end
 end
