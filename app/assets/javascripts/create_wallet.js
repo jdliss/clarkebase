dc461ff10db846eb1c1609d4983486729f2ad908
Matt Pindell <pindell.m@gmail.com>
2016-06-07 19:21:51 -0600

    Add AJAX call for removing 'Create Wallet' button

--- app/assets/javascripts/create_wallet.js
+++ app/assets/javascripts/create_wallet.js
@@ -6,7 +6,13 @@ $(document).ready(function(){
       url:      '/api/v1/wallets',
       dataType: 'JSON',
       success: function(data){
-        $(self).parent().hide()
+        $(self).parent().hide();
+        $('.flash').empty();
+        $('.flash').append('<div class="alert alert-success">Your Wallet has been created!</div>');
+      },
+      error: function(data){
+        $('.flash').empty();
+        $('.flash').append('<div class="alert alert-danger">Wallet not created, try again.</div>');
       }
     });
   })
