9c4f4cfa272b0a72c123ca7e7c3dcb025ce5074b
Matt Pindell <pindell.m@gmail.com>
2016-06-07 19:23:58 -0600

    Add public_key method to PKeyService

--- app/services/pkey_service.rb
+++ app/services/pkey_service.rb
@@ -6,4 +6,9 @@ class PKeyService
     keypair = OpenSSL::PKey::RSA.generate(2048)
     keypair.to_der
   end
+
+  def self.public_key(pkey)
+    key = OpenSSL::PKey::RSA.new(pkey)
+    key.public_key.to_der
+  end
 end
