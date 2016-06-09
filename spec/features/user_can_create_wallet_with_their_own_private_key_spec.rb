ca27d4bb3729683cea2fb9b5c1ba43c80da11213
Jonathan <jonacom@lissismore.com>
2016-06-08 19:35:15 -0600

    Finish test for using a custom private key

--- spec/features/user_can_create_wallet_with_their_own_private_key_spec.rb
+++ spec/features/user_can_create_wallet_with_their_own_private_key_spec.rb
@@ -3,16 +3,15 @@ require 'rails_helper'
 RSpec.feature "User can create a wallet" do
   xscenario "a registered user with no wallet is offered to create a wallet", js: true do
     user = create(:user)
-    login_as user, scope: :user
 
+    login_as user, scope: :user
     visit wallets_new_path
 
     expect(user.wallet).to eq(nil)
 
     within(".new-wallet-message") do
       expect(page).to have_content "You Need a Wallet"
-
-
+      fill_in "Private Key:", with: ENV["PRIVATE_KEY"]
       click_button "Create Wallet"
     end
 
@@ -20,9 +19,8 @@ RSpec.feature "User can create a wallet" do
 
     click_button "Take Me to My Dashboard"
 
-
     expect(current_page).to eq dashboard_path
-
     expect(user.wallet).to exist
+    expect(Base64.strict_encode64(user.wallet.private_key)).to eq EVN["PRIVATE_KEY"]
   end
 end
