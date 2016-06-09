1d0ff20e98e3177196e0595ec2b60b124d44a3d3
Jonathan <jonacom@lissismore.com>
2016-06-08 19:23:48 -0600

    Implement import key for wallet creation

--- app/assets/javascripts/create_wallet.js
+++ app/assets/javascripts/create_wallet.js
@@ -5,15 +5,33 @@ $(document).ready(function(){
       method:   'POST',
       url:      '/api/v1/wallets',
       dataType: 'JSON',
-      success: function(data){
-        $('.flash').empty();
-        $('.flash').append('<div class="alert text-center alert-success">Your Wallet has been created!</div>');
-        $(".new-wallet").html('<a href="/dashboard" class="btn btn-success btn-lg btn-block dash-button">Take Me to My Dashboard</button>');
-      },
-      error: function(data){
-        $('.flash').empty();
-        $('.flash').append('<div class="alert text-center alert-danger">Wallet not created, try again.</div>');
-      }
-    });
-  })
+      success:  flashSuccess,
+      error:    flashError
+    })
+  });
+
+  $('#submit-key').submit(function(e){
+    event.preventDefault();
+    var self = this;
+    var value = $("#submit-key").serialize();
+    $.ajax({
+      method:   'POST',
+      url:      '/api/v1/wallets',
+      data:     value,
+      dataType: 'JSON',
+      success:  flashSuccess,
+      error:    flashError
+    })
+  });
+
+    function flashSuccess(data){
+      $('.flash').empty();
+      $('.flash').append('<div class="alert text-center alert-success">Your Wallet has been created!</div>');
+      $(".new-wallet").html('<a href="/dashboard" class="btn btn-success btn-lg btn-block dash-button">Take Me to My Dashboard</button>');
+    }
+
+    function flashError(data){
+      $('.flash').empty();
+      $('.flash').append('<div class="alert text-center alert-danger">Wallet not created, try again.</div>');
+    }
 })
---
+++ spec/features/user_can_create_wallet_with_their_own_private_key_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "User can create a wallet" do
+  xscenario "a registered user with no wallet is offered to create a wallet", js: true do
+    user = create(:user)
+    login_as user, scope: :user
+
+    visit wallets_new_path
+
+    expect(user.wallet).to eq(nil)
+
+    within(".new-wallet-message") do
+      expect(page).to have_content "You Need a Wallet"
+
+
+      click_button "Create Wallet"
+    end
+
+    wait_for_ajax
+
+    click_button "Take Me to My Dashboard"
+
+
+    expect(current_page).to eq dashboard_path
+
+    expect(user.wallet).to exist
+  end
+end
