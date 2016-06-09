2f46c7a99607d92b0fb00a49b45e1cb574c5b81d
Matt Pindell <pindell.m@gmail.com>
2016-06-08 19:47:53 -0600

    Refactor clean_public_key

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -7,24 +7,23 @@ class Wallet < ActiveRecord::Base
   def generate_keys
     key_service = PKeyService.new
     self.private_key ||= key_service.generate_private_key
-    self.public_key ||= key_service.public_key
+    self.public_key  ||= key_service.public_key
   end
 
   def address
-    self.public_key.gsub("\n", "")
-    # remove "----BEGIN"
+    clean_public_key
   end
 
   def balance
     clarke_service.parsed_balance(address)
   end
 
-  def self.clean_input_key(key)
+  def clean_public_key
+    key = self.public_key
     key.slice!("-----BEGIN RSA PRIVATE KEY-----")
     key.slice!("-----END RSA PRIVATE KEY-----")
     key.gsub!("\\n", "")
     key.gsub!("\n", "")
-    Base64.decode64(key)
   end
 
 private
