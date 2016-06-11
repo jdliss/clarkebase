7afeb2d15f6df97127ef4a78aa21fb0727e72c5e
Jonathan <jonacom@lissismore.com>
2016-06-11 13:28:27 -0600

    Add friends to activerecord

---
+++ app/controllers/api/v1/address_book_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::AddressBookController < ApiController
+  def update
+    require "pry"; binding.pry
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,12 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
 
   has_one :wallet
+  has_and_belongs_to_many :friends,
+    class_name: "User",
+    join_table: :friendships,
+    foreign_key: :user_id,
+    association_foreign_key: :friend_user_id
 end
