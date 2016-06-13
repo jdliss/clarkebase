9d43a612d43ed2724bcbc8ac5980c666dc7e7736
Matt Pindell <pindell.m@gmail.com>
2016-06-13 15:09:41 -0600

    Refactor dashboard form to simultaneously accept name and private key or only name

--- app/assets/javascripts/create_wallet.js
+++ app/assets/javascripts/create_wallet.js
@@ -9,40 +9,29 @@ $(document).ready(function(){
     }
   });
 
-  $('#name-wallet').submit(function(e){
+  $('#wallet-form').submit(function(e){
     event.preventDefault();
     var self = this;
-    var value = $("#name-wallet").serialize();
+    var data = $("#wallet-form").serialize();
     $.ajax({
       method:   'POST',
       url:      '/api/v1/wallets',
-      data:     value,
+      data:     data,
       dataType: 'JSON',
-      success:  appendWallet(value),
-      error:    flashError
-    })
-  });
-
-  $('#submit-key').submit(function(e){
-    event.preventDefault();
-    var self = this;
-    var value = $("#submit-key").serialize();
-    $.ajax({
-      method:   'POST',
-      url:      '/api/v1/wallets',
-      data:     value,
-      dataType: 'JSON',
-      success:  flashSuccess,
+      success:  appendWallet(data),
       error:    flashError
     })
   });
 
     function appendWallet(data){
-      var nameData  = data.split("=");
-      var nameArray = nameData[nameData.length - 1];
-      var name      = nameArray.split("+").join(" ");
-      var href      = '<a href="/dashboard/wallets/' + parameterize(name) + '">' + name + '</a>'
-      $('.wallets').append('<span class="card card-block sidebar-wallet"><a>' + href + '</a><span class="pull-right">0 CLC</span></span>');
+      var walletData = data.split("&");
+      var walletName = walletData[0]
+      var nameData   = walletName.split("=");
+      var nameArray  = nameData[nameData.length - 1];
+      var name       = nameArray.split("+").join(" ");
+
+      var href       = '<a href="/dashboard/wallets/' + parameterize(name) + '">' + name + '</a>'
+      $('.wallets').append('<span class="card card-block sidebar-wallet"><a>' + href + '</a><span class="pull-right">pending</span></span>');
       $('#dash-create-wallet').modal('hide');
       flashSuccess;
     }
@@ -57,7 +46,14 @@ $(document).ready(function(){
       $('.flash').append('<div class="alert text-center alert-danger">Wallet not created, try again later.</div>');
     }
 
-    parameterize = function (str1) {
-      return str1.trim().toLowerCase().replace(/[^a-zA-Z0-9 -]/, "").replace(/\s/g, "-");
+    parameterize = function (string) {
+      return string.trim().toLowerCase().replace(/[^a-zA-Z0-9 -]/, "").replace(/\s/g, "-");
     };
+
+    nameFormatter = function (string) {
+      var nameData  = string.split("=");
+      var nameArray = nameData[nameData.length - 1];
+      var name      = nameArray.split("+").join(" ");
+      return name;
+    }
 })
--- app/controllers/api/v1/wallets_controller.rb
+++ app/controllers/api/v1/wallets_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::WalletsController < ApiController
   def create
-    if params.dig("private_key")
+    if params.dig("private_key") && !params.dig("private_key").empty?
       @private_key = KeyCleanerService.clean_user_input(params.dig("private_key"))
     end
 
     @wallet = Wallet.new(
       user_id:     current_user.id,
       private_key: @private_key,
-      name:        params["wallet_name"]
+      name:        params.dig("wallet_name")
     )
 
     if @wallet.save
