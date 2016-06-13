e0b4fc826864b4060d29d8e749429586c94f7603
Jonathan <jonacom@lissismore.com>
2016-06-13 14:28:40 -0600

    append new friend to list after ajax call is made

--- app/assets/javascripts/pay-friend.js
+++ app/assets/javascripts/pay-friend.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $('.toggle-pay-modal').on('click', function() {
     event.preventDefault();
-    var email = $(this).attr("data-email");
-    var address = $(this).attr("data-address");
+    var email = $(".toggle-address-modal").attr("data-email");
+    var address = $(".toggle-address-modal").attr("data-address");
 
     $("#pay-to-header").empty();
     $("#pay-to-header").append("Pay " + email)
