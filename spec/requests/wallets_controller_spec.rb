898544f4ab10180caa6647ee2a5db208bca5165f
Jonathan <jonacom@lissismore.com>
2016-06-14 17:00:07 -0600

    api wallets controller (given key)

--- spec/requests/wallets_controller_spec.rb
+++ spec/requests/wallets_controller_spec.rb
@@ -8,10 +8,28 @@ RSpec.describe Api::V1::WalletsController do
     expect(user.wallets).to eq []
     expect(user.primary_wallet).to eq nil
 
-    post api_v1_wallets_path, format: :json, wallet_name: "money wife doesn't know about"
+    post api_v1_wallets_path, format: :json,
+      wallet_name: "money wife doesn't know about"
 
     expect(user.wallets.count).to eq 1
     expect(user.primary_wallet).to_not eq nil
     expect(user.primary_wallet.name).to eq "money wife doesn't know about"
+  end
+
+  it "creates a new wallet and uses given key" do
+    user = create(:user)
+    login_as user
+
+    expect(user.wallets).to eq []
+    expect(user.primary_wallet).to eq nil
+
+    post api_v1_wallets_path, format: :json,
+      wallet_name: "money wife does know about",
+      private_key: ENV["PRIVATE_KEY"]
+
+    expect(user.wallets.count).to eq 1
+    expect(user.primary_wallet).to_not eq nil
+    expect(user.primary_wallet.name).to eq "money wife does know about"
+    expect(user.primary_wallet.private_key).to eq ENV["PRIVATE_KEY"]
   end
 end
