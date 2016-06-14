bb266d3875da0fb5cf4300501e4a2ed2941e4519
Matt Pindell <pindell.m@gmail.com>
2016-06-13 21:01:50 -0600

    Add wallet modal dissapears on success

--- app/assets/javascripts/create_wallet.js
+++ app/assets/javascripts/create_wallet.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 
   $('#submitName').prop('disabled', true);
 
-  $('#wallet_name').keyup(function() {
-    var input = $("#wallet_name").val();
-    if(input != "") {
-      $('#submitName').prop('disabled', false);
-    }
-  });
+  // $('#wallet_name').keyup(function() {
+  //   var input = $("#wallet_name").val();
+  //   if(input != "") {
+  //     $('#submitName').prop('disabled', false);
+  //   }
+  // });
 
   $('#wallet-form').submit(function(e){
     event.preventDefault();
@@ -27,28 +27,20 @@ $(document).ready(function(){
       $.ajax({
         method:   'GET',
         url:      '/dashboard.js',
-        success:  flashSuccess
+        success:  flashSuccess,
+        error:    flashError
       });
     }
 
     function flashSuccess(data){
+      $('#dash-create-wallet').modal("hide");
       $('.flash').empty();
       $('.flash').append('<div class="alert text-center alert-success">Your Wallet has been created!</div>');
     }
 
     function flashError(data){
+      $('#dash-create-wallet').modal("hide");
       $('.flash').empty();
       $('.flash').append('<div class="alert text-center alert-danger">Wallet not created, try again later.</div>');
     }
-
-    // parameterize = function (string) {
-    //   return string.trim().toLowerCase().replace(/[^a-zA-Z0-9 -]/, "").replace(/\s/g, "-");
-    // };
-    //
-    // nameFormatter = function (string) {
-    //   var nameData  = string.split("=");
-    //   var nameArray = nameData[nameData.length - 1];
-    //   var name      = nameArray.split("+").join(" ");
-    //   return name;
-    // }
 })
