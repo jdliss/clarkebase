f4a4b0ef37317969310c6bb131ebd10373a2b745
Matt Pindell <pindell.m@gmail.com>
2016-06-14 13:49:37 -0600

    Add modal removal to success functions

--- app/assets/javascripts/create_wallet.js
+++ app/assets/javascripts/create_wallet.js
@@ -2,13 +2,6 @@ $(document).ready(function(){
 
   $('#submitName').prop('disabled', true);
 
-  // $('#wallet_name').keyup(function() {
-  //   var input = $("#wallet_name").val();
-  //   if(input != "") {
-  //     $('#submitName').prop('disabled', false);
-  //   }
-  // });
-
   $('#wallet-form').submit(function(e){
     event.preventDefault();
     var self = this;
@@ -33,13 +26,13 @@ $(document).ready(function(){
     }
 
     function flashSuccess(data){
-      $('#dash-create-wallet').modal("hide");
+      $('#dash-create-wallet').modal('hide')
       $('.flash').empty();
       $('.flash').append('<div class="alert text-center alert-success">Your Wallet has been created!</div>');
     }
 
     function flashError(data){
-      $('#dash-create-wallet').modal("hide");
+      $('#dash-create-wallet').modal('hide')
       $('.flash').empty();
       $('.flash').append('<div class="alert text-center alert-danger">Wallet not created, try again later.</div>');
     }
