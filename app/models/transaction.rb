70fbe91005682c5193fb89db48aa8c4e8b0cfbd1
Matt Pindell <pindell.m@gmail.com>
2016-06-14 16:40:41 -0600

    Create pares_current_spending method and corresponding test

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,13 +4,11 @@ class Transaction < ActiveRecord::Base
 
   enum status: %w(pending completed failed)
 
-
   def send_transaction
     pkey     = find_pkey(self.from)
     service  = ClarkeService.new(pkey)
     unsigned = service.parsed_unsigned_payment(self.from, self.to, self.amount)
     signed   = service.parsed_signed_payment(unsigned)
-
   end
 
   def email(whom)
@@ -25,8 +23,26 @@ class Transaction < ActiveRecord::Base
     self.created_at.strftime("%a %b %d, %Y -%l:%M %p")
   end
 
+  def update_transaction
+    pending_transactions = transaction_service.parse_current_pending
+
+    still_pending = pending_transactions.map do |pending|
+      Transaction.pending.find_by(from: pending[:from], to: pending[:to])
+    end
+
+    not_pending = Transaction.pending - still_pending
+
+    not_pending.each do |transaction|
+      transaction.completed!
+    end
+  end
+
   private
     def find_pkey(address)
       Wallet.find_by(public_key: address).private_key
+    end
+
+    def transaction_service
+      TransactionUpdateService.new
     end
 end
---
+++ app/services/transaction_update_service.rb
@@ -0,0 +1,32 @@
+class TransactionUpdateService
+
+  def initialize(key=nil)
+    @sigkey  = key
+    @connection = Faraday.new(url: get_node)
+  end
+
+  def get_node
+    service = NodeService.new
+    active_nodes = service.parse_node
+    node = service.get_url(active_nodes)
+  end
+
+  def current_pending
+    @connection.get do |req|
+      req.url '/pending_transactions'
+      req.headers['Content-Type'] = 'application/json'
+    end
+  end
+
+  def parse_current_pending
+    transactions = JSON.parse(current_pending.body)
+
+    unless transactions.empty?
+      result = transactions['payload'].map do |payload_item|
+        data = payload_item['outputs'].flatten
+        { from: data.first['address'], to: data.last['address'] }
+      end
+    end
+  end
+
+end
