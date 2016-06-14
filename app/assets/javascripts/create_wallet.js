339422ccd2d7319ea3f12c691a3efa54720d8fff
Matt Pindell <pindell.m@gmail.com>
2016-06-13 20:50:10 -0600

    Removed unneeded js content

--- app/assets/javascripts/create_wallet.js
+++ app/assets/javascripts/create_wallet.js
@@ -18,32 +18,22 @@ $(document).ready(function(){
       url:      '/api/v1/wallets',
       data:     data,
       dataType: 'JSON',
-      success:  flashSuccess,
+      success:  appendWallet,
       error:    flashError
     })
   });
 
     function appendWallet(data){
-      var walletData = data.split("&");
-      var walletName = walletData[0]
-      var nameData   = walletName.split("=");
-      var nameArray  = nameData[nameData.length - 1];
-      var name       = nameArray.split("+").join(" ");
-
-      var href       = '<a href="/dashboard/wallets/' + parameterize(name) + '">' + name + '</a>'
-      $('.wallets').append('<span class="card card-block sidebar-wallet"><a>' + href + '</a><span class="pull-right">pending</span></span>');
-      $('#dash-create-wallet').modal('hide');
-      flashSuccess;
+      $.ajax({
+        method:   'GET',
+        url:      '/dashboard.js',
+        success:  flashSuccess
+      });
     }
 
     function flashSuccess(data){
       $('.flash').empty();
       $('.flash').append('<div class="alert text-center alert-success">Your Wallet has been created!</div>');
-
-      $.ajax({
-        method:   'GET',
-        url:      '/dashboard.js'
-      });
     }
 
     function flashError(data){
@@ -51,14 +41,14 @@ $(document).ready(function(){
       $('.flash').append('<div class="alert text-center alert-danger">Wallet not created, try again later.</div>');
     }
 
-    parameterize = function (string) {
-      return string.trim().toLowerCase().replace(/[^a-zA-Z0-9 -]/, "").replace(/\s/g, "-");
-    };
-
-    nameFormatter = function (string) {
-      var nameData  = string.split("=");
-      var nameArray = nameData[nameData.length - 1];
-      var name      = nameArray.split("+").join(" ");
-      return name;
-    }
+    // parameterize = function (string) {
+    //   return string.trim().toLowerCase().replace(/[^a-zA-Z0-9 -]/, "").replace(/\s/g, "-");
+    // };
+    //
+    // nameFormatter = function (string) {
+    //   var nameData  = string.split("=");
+    //   var nameArray = nameData[nameData.length - 1];
+    //   var name      = nameArray.split("+").join(" ");
+    //   return name;
+    // }
 })
