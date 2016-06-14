c316d04796ba84d6bb252700e5d826ceac9b424f
Jonathan <jonacom@lissismore.com>
2016-06-14 17:22:55 -0600

    address books controller request spec (email and public key)

--- spec/requests/address_books_controller_spec.rb
+++ spec/requests/address_books_controller_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe Api::V1::AddressBooksController do
     expect(user.friends).to include(user2)
   end
 
+  it "adds creates a friendship when given a public key" do
+    user, user2 = create_list(:user, 2)
+    wallet = create(:wallet, user_id: user2.id)
+    wallet.primary!
+    login_as user
+
+    expect(user.friends).to_not include(user2)
+
+    patch api_v1_address_books_path, format: :json, key: user2.primary_wallet.address
+
+    expect(user.friends).to include(user2)
+  end
 end
