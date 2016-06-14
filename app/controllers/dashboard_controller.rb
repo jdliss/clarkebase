273332bd5b9f4c03bf6cbfe101dedeba20819a29
Matt Pindell <pindell.m@gmail.com>
2016-06-13 20:52:59 -0600

    Refactor to use application_controller methods

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,7 @@ class DashboardController < ApplicationController
   def show
     @sent_transactions     = Wallet.all_sent_transactions(@wallets)
     @received_transactions = Wallet.all_received_transactions(@wallets)
-    @recently_added_wallet = current_user.wallets.order('created_at').last
+    @recently_added_wallet = @wallets.order('created_at').last
 
     respond_to do |format|
       format.js { render 'show_balance' }
